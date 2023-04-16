f6a3a4fe5474b3bdcfff2e45df58469e55750418
ImadQADDOURI <imad.qaddouri@ump.ac.ma>
2023-04-16 17:05:51 +0000

    defaultdata setup

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const mongoose =require("mongoose");
 const app = express();
+const cors = require('cors');
 
 require('./DB/conn');
 
 const Products = require("./models/productsSchema");
-const Default
+const DefaultData = require("./defaultdata");
+
+
+app.use(express.json());
+app.use(cors());
+
 
 
 const port = 8005;
 
 
 app.listen(port,()=>{console.log("server runing on port "+ port ); });
+
+
+DefaultData;
---
+++ routes/router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = new express.Router();
+const Products = require("../models/productsSchema");
+
+
+
+
+module.exports = router;
