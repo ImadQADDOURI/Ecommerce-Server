fb317cffc75da40a364f18aec0acaf90457c1cc1
ImadQADDOURI <imad.qaddouri@ump.ac.ma>
2023-04-17 16:53:33 +0000

    router GET /getproducts

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ require('./DB/conn');
 const Products = require("./models/productsSchema");
 const DefaultData = require("./defaultdata");
 
+const router = require("./routes/router");
 
 app.use(express.json());
 app.use(cors());
-
+app.use(router);
 
 
 const port = 8005;
--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,18 @@ const router = new express.Router();
 const Products = require("../models/productsSchema");
 
 
+router.get("/getproducts",async(red,res)=>{
+try {
+    const productsdata = await Products.find();
+    console.log("***************************************************** router.get(getproducts) "+productsdata);
+    res.status(201).json(productsdata);
+} catch (error) {
+    console.log(error.message);
+}
+
+})
+
+
 
 
 module.exports = router;
